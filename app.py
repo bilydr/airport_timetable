407ae112d39fa56d27d389c3b5cb1b5aa5cb6892
Longyi Bi <lbi@carlsonwagonlit.com>
2019-07-22 17:36:02 -0500

    update gitignore, add home.html

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('./home.html')
+    airport = "MSP"
+    return render_template('./home.html', airport=airport)
+
 
 @app.route('/info')
 def info():
     return '<h1>About</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
