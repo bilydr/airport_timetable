085c97ea5994510a32e3a8b5e5c19cd5af1041fd
Longyi Bi <lbi@carlsonwagonlit.com>
2019-07-22 22:46:00 -0500

    add bootstrap

--- app.py
+++ app.py
@@ -7,13 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    airport = "MSP"
-    return render_template('./home.html', airport=airport)
+    airports = ['MSP', 'CDG', 'SFO', 'DFW', 'SEA', 'BWI']
+    return render_template('home.html', airports=airports)
+
+@app.route('/lookup')
+def lookup():
+    return "<h3>Your airport's timetable will appear here</h3>"
+
+@app.route('/top')
+def top_airport():
+    return "<h3>top airports will appear here</h3>"
 
 
-@app.route('/info')
+@app.route('/about')
 def info():
-    return '<h1>About</h1>'
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
